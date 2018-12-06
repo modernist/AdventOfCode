b48a9190d3743b3913d4472bb80c5a2de3f09bf3
Kostas Stroggylos <modernist@users.noreply.github.com>
2018-12-06 23:46:35 +0200

    Day 6

--- 2018/Day6/Program.cs
+++ 2018/Day6/Program.cs
@@ -14,18 +14,81 @@ namespace Day6
                 .Select(coords => new Point(int.Parse(coords[0]), int.Parse(coords[1])));
 
             Console.WriteLine(Part1(points));
-            Console.Read();
+            Console.WriteLine(Part2(points));
         }
 
         static int Part1(IEnumerable<Point> points)
         {
-            var areas = points.ToDictionary(point => point, point => -1);
+            var areas = points.ToDictionary(point => point, point => 0);
+            //extend the grid by 1 in each direction
+            var left = points.Min(point => point.X) - 1;
+            var right = points.Max(point => point.X) + 1;
+            var top = points.Min(point => point.Y) - 1;
+            var bottom = points.Max(point => point.Y) + 1;
 
+            for (var x = left; x <= right; x++)
+            {
+                for (var y = top; y <= bottom; y++)
+                {
+                    var minDistance = points.Select(point => point.ManhattanDistance(x, y)).Min();
+                    var candidates = points.Where(point => point.ManhattanDistance(x, y) == minDistance).ToArray();
 
-            return 0;
+                    if (candidates.Length != 1)
+                    {
+                        continue; //break ties
+                    }
+
+                    //if the current location is adjacent to the end of the grid the point's area is infinite
+                    if (x == left || x == right || y == top || y == bottom)
+                    {
+                        foreach (var candidate in candidates)
+                        {
+                            areas[candidate] = int.MaxValue;
+                        }
+                    }
+                    else
+                    {
+                        foreach (var candidate in candidates)
+                        {
+                            if (areas[candidate] != int.MaxValue)
+                            {
+                                areas[candidate]++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return areas.Values.Where(v => v != int.MaxValue).Max();
         }
 
-        class Point
+        static int Part2(IEnumerable<Point> points)
+        {
+            //extend the grid by 1 in each direction
+            var left = points.Min(point => point.X) - 1;
+            var right = points.Max(point => point.X) + 1;
+            var top = points.Min(point => point.Y) - 1;
+            var bottom = points.Max(point => point.Y) + 1;
+
+            var region = 0;
+
+            for (var x = left; x <= right; x++)
+            {
+                for (var y = top; y <= bottom; y++)
+                {
+                    var totalDistance = points.Select(point => point.ManhattanDistance(x, y)).Sum();
+                    if (totalDistance < 10000)
+                    {
+                        //(x, y) should be included in the region
+                        region++;
+                    }
+                }
+            }
+
+            return region;
+        }
+
+        class Point : IEquatable<Point>
         {
             public int X { get; }
 
@@ -40,6 +103,19 @@ namespace Day6
             public int ManhattanDistance(int x, int y)
             {
                 return Math.Abs(X - x) + Math.Abs(Y - y);
+            }
+
+            public bool Equals(Point other)
+            {
+                return other != null && X == other.X && Y == other.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return X * 65535 + Y;
+                }
             }
         }
     }
