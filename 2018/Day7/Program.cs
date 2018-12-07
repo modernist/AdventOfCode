898ba00144a7476699decf9247b71d314c4ab0cf
Kostas Stroggylos <modernist@users.noreply.github.com>
2018-12-07 23:01:26 +0200

    Day 7 Part 2

--- 2018/Day7/Program.cs
+++ 2018/Day7/Program.cs
@@ -11,19 +11,20 @@ namespace Day7
         {
             var input = File.ReadAllLines("input.txt");
             var edges = input.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
-                .Select(tokens => (Before: tokens[1], After: tokens[7]));
+                .Select(tokens => (Before: tokens[1][0], After: tokens[7][0]));
             var vertices = edges.SelectMany(edge => new[] {edge.Before, edge.After}).ToHashSet();
 
-            var part1 = string.Join("", Part1(vertices, edges.ToHashSet()));
+            var part1 = new string(Part1(vertices, edges.ToHashSet()).ToArray());
             Console.WriteLine(part1);
-
+            var part2 = Part2(vertices, edges, 5);
+            Console.WriteLine(part2);
         }
 
-        static IEnumerable<string> Part1(HashSet<string> vertices, HashSet<(string Before, string After)> edges)
+        static IEnumerable<char> Part1(IEnumerable<char> vertices, HashSet<(char Before, char After)> edges)
         {
-            var sorted = new List<string>();
+            var sorted = new List<char>();
             //find all vertices with no incoming edges
-            var eligible = new SortedSet<string>(vertices.Where(vertex => edges.All(edge => edge.After != vertex)));
+            var eligible = new SortedSet<char>(vertices.Where(vertex => edges.All(edge => edge.After != vertex)));
             while (eligible.Any())
             {
                 var vertex = eligible.First();
@@ -46,11 +47,50 @@ namespace Day7
             if (edges.Any())
             {
                 // graph contains cycles
-                return new [] { "cycle detected"};
+                return "cycle detected";
             }
 
             return sorted;
         }
 
+        static int Part2(IEnumerable<char> vertices, IEnumerable<(char Before, char After)> edges, int workerCount)
+        {
+            var time = 0;
+            var workers = new int[workerCount];
+            var tasks = new SortedSet<char>(vertices);
+            var done = new List<(char Task, int Finish)>();
+            var dependencies = edges.ToList();
+
+            while (tasks.Any() || workers.Any(w => w > time))
+            {
+                foreach (var d in done.Where(task => task.Finish <= time))
+                {
+                    dependencies.RemoveAll(dependency => dependency.Before == d.Task);
+                }
+
+                done.RemoveAll(task => task.Finish <= time);
+                // find all tasks that can be started now (no dependencies)
+                var eligible = tasks.Where(task => dependencies.All(d => d.After != task)).ToList();
+
+                for (var worker = 0; worker < workerCount && eligible.Any(); worker++)
+                {
+                    if (workers[worker] <= time) //worker is idle
+                    {
+                        var task = eligible.First();
+                        var finish = Cost(task) + time;
+                        workers[worker] = finish;
+                        tasks.Remove(task);
+                        done.Add((task, finish));
+                        eligible.Remove(task);
+                    }
+                }
+
+                time++;
+            }
+
+            return time;
+        }
+
+        static int Cost(char step) => step - 4; // 'A' = 65, Cost('A') = 61
     }
 }
