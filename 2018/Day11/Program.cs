c6020d0ab65e4b21dd65b2dbede276c8d1af8f77
Kostas Stroggylos <kostas.stroggylos@e-food.gr>
2018-12-11 10:26:05 +0200

    Day 11 part 1

---
+++ 2018/Day11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int serial = 9424, width = 300, height = 300;
+            var grid = BuildGrid(width, height, serial);
+            Console.WriteLine(Part1(grid, width, height, 3));
+        }
+
+        static long[,] BuildGrid(int width, int height, int serial)
+        {
+            long[,] grid = new long [width + 1, height + 1];
+            for (long x = 1; x <= width; x++)
+            {
+                for (long y = 1; y <= height; y++)
+                {
+                    var rackId = x + 10;
+                    grid[x, y] = ((rackId * y + serial) * rackId) % 1000 / 100 - 5;
+                }
+            }
+            return grid;
+        }
+
+        static (int x, int y) Part1(long[,] grid, int width, int height, int edge)
+        {
+            var squares = new Dictionary<(int x, int y), long>();
+            var maxLocation = (x: 0, y: 0);
+            long maxPower = long.MinValue;
+            for (int x = 1; x <= width - 2; x++)
+            {
+                for (int y = 1; y <= height - 2; y++)
+                {
+                    var location = (x, y);
+                    squares[location] = 0;
+                    for (int i = x; i < x + edge; i++)
+                    {
+                        for (int j = y; j < y + edge; j++)
+                        {
+                            squares[(x, y)] += grid[i, j];
+                        }
+                    }
+
+                    if (squares[location] > maxPower)
+                    {
+                        maxPower = squares[location];
+                        maxLocation = location;
+                    }
+                }
+            }
+
+            return maxLocation;
+        }
+
+    }
+}
