f67166f39f5691e285eead7eaf84a9ac850511d7
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-02 00:38:39 +0200

    Day 01 part 1

---
+++ 2019/aoc2019/Day01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+
+            Console.WriteLine(Part1(input));
+
+            Console.Read();
+        }
+
+        static int Part1(int[] modules)
+        {
+            return modules.Aggregate(0, (total, current) => total + ModuleFuel(current));
+        }
+
+        static int ModuleFuel(int i)
+        {
+            return (int)Math.Floor(i / 3.0) - 2;
+        }
+    }
+}
