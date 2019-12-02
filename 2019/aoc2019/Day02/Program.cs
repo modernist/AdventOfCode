0f33ae8ede8666867c3213eab05b22a6f118ac8a
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-02 20:43:16 +0200

    Day 2 part 2

--- 2019/aoc2019/Day02/Program.cs
+++ 2019/aoc2019/Day02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,46 +10,53 @@ namespace Day02
         static void Main(string[] args)
         {
             var input = File.ReadAllText("input.txt").Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToArray();
+                .Select(int.Parse);
 
-            Console.WriteLine(Part1(input));
+            Console.WriteLine(Part1(input.ToArray(), 12, 2));
+
+            Console.WriteLine(Part2(input));
 
             Console.Read();
         }
 
-        static int Part1(int[] input)
+        static int Part1(int[] input, int noun = 12, int verb = 2)
         {
-            //prepare input
-            var pos1 = input[1]; 
-            var pos2 = input[2];
-            input[1] = 12;
-            input[2] = 2;
-
-            // test input = new[] {1, 1, 1, 4, 99, 5, 6, 0, 99};
+            input[1] = noun;
+            input[2] = verb;
 
             int opCodePos = 0;
             while (input[opCodePos] != 99)
             {
-                Step(input, ref opCodePos);
+                switch (input[opCodePos])
+                {
+                    case 1: //add
+                        input[input[opCodePos + 3]] = input[input[opCodePos + 1]] + input[input[opCodePos + 2]];
+                        opCodePos += 4;
+                        break;
+
+                    case 2: //multiply
+                        input[input[opCodePos + 3]] = input[input[opCodePos + 1]] * input[input[opCodePos + 2]];
+                        opCodePos += 4;
+                        break;
+                }
             }
 
             return input[0];
         }
 
-        static void Step(int[] input, ref int opCodePos)
+        static int Part2(IEnumerable<int> input)
         {
-            switch (input[opCodePos])
+            for (var noun = 0; noun < 100; noun++)
             {
-                case 1: //add
-                    input[input[opCodePos + 3]] = input[input[opCodePos + 1]] + input[input[opCodePos + 2]];
-                    opCodePos += 4;
-                    break;
-
-                case 2: //multiply
-                    input[input[opCodePos + 3]] = input[input[opCodePos + 1]] * input[input[opCodePos + 2]];
-                    opCodePos += 4;
-                    break;
+                for (var verb = 0; verb < 100; verb++)
+                {
+                    var result = Part1(input.ToArray(), noun, verb);
+                    if (result == 19690720)
+                        return noun * 100 + verb;
+                }
             }
+
+            return 0;
         }
     }
 }
