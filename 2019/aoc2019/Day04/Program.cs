9b5eb993456a96f9574e26ec4d3e397f34ffef39
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-04 23:23:56 +0200

    Day 04 Part 2

--- 2019/aoc2019/Day04/Program.cs
+++ 2019/aoc2019/Day04/Program.cs
@@ -12,6 +12,8 @@ namespace Day04
 
             Console.WriteLine(Part1(start, end));
 
+            Console.WriteLine(Part2(start, end));
+
             Console.Read();
         }
 
@@ -27,6 +29,25 @@ namespace Day04
                     return (new string(nsa) == ns) && nsa.Distinct().Count() <= 5;
                 });
 
+
+            return numbers.Count();
+        }
+
+        static int Part2(int min, int max)
+        {
+            var numbers = Enumerable.Range(min, max - min + 1)
+                .Select(n => n.ToString())
+                .Where(ns =>
+                {
+                    var nsa = ns.ToArray();
+                    Array.Sort(nsa);
+
+                    if (new string(nsa) != ns)
+                        return false;
+
+                    var groups = nsa.GroupBy(c => c);
+                    return groups.Any(g => g.Count() == 2) && groups.Count() <= 5;
+                });
 
             return numbers.Count();
         }
