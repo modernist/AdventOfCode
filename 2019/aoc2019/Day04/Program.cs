d8338577e4bde0df6c2dd9f6243fddab0e02245f
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-04 23:02:43 +0200

    Day 04 Part 1

---
+++ 2019/aoc2019/Day04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = 264793;
+            var end = 803935;
+
+            Console.WriteLine(Part1(start, end));
+
+            Console.Read();
+        }
+
+        static int Part1(int min, int max)
+        {
+            var numbers = Enumerable.Range(min, max - min + 1)
+                .Select(n => n.ToString())
+                .Where(ns =>
+                {
+                    var nsa = ns.ToArray();
+                    Array.Sort(nsa);
+
+                    return (new string(nsa) == ns) && nsa.Distinct().Count() <= 5;
+                });
+
+
+            return numbers.Count();
+        }
+    }
+}
