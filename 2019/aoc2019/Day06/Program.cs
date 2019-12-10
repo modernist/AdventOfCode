0a81b5f6052e774463461b3bb45c28aa85094050
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-10 23:25:45 +0200

    Day 06 Part 1

---
+++ 2019/aoc2019/Day06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt")
+                .Select(l => (Inner: l.Substring(0, 3), Outer: l.Substring(4)))
+                .ToDictionary(orbit => orbit.Outer, orbit => orbit.Inner); ;
+
+            Console.WriteLine(Part1(input));
+
+            Console.Read();
+        }
+
+        static int Part1(IDictionary<string, string> orbits)
+        {
+            return orbits.Keys.Select(item => TraverseOrbits(orbits, item).Count()).Sum();
+        }
+
+        static IEnumerable<string> TraverseOrbits(IDictionary<string, string> orbits, string item)
+        {
+            for (var inner = orbits[item]; inner != null; orbits.TryGetValue(inner, out inner))
+            {
+                yield return inner;
+            }
+        }
+    }
+}
