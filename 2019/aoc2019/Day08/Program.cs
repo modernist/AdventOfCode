cc59c63468bf34e00e64cfeb62c29a4c4b9cc751
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-12 03:02:31 +0200

    Day 08 Part 2

--- 2019/aoc2019/Day08/Program.cs
+++ 2019/aoc2019/Day08/Program.cs
@@ -14,6 +14,8 @@ namespace Day08
 
             Console.WriteLine(Part1(images));
 
+            Part2(images, 25);
+
             Console.Read();
         }
 
@@ -22,6 +24,52 @@ namespace Day08
             var targetImage = images.OrderBy(i => i.Count(px => px == 0)).First();
 
             return targetImage.Count(px => px == 1) * targetImage.Count(px => px == 2);
+        }
+
+        static void Part2(IEnumerable<int>[] images, int width)
+        {
+            var final = new List<Pixel>();
+            var size = images[0].Count();
+            final.AddRange(Enumerable.Range(0, size).Select(_ => new Pixel()));
+
+            for (var i = images.Length - 1; i >= 0; i--)
+            {
+                var image = images[i].ToArray();
+                for (var px = 0; px < size; px++)
+                {
+                    final[px].Apply(image[px]);
+                }
+            }
+
+            var finalImage = Partition(final, width);
+            foreach (var line in finalImage)
+            {
+                Console.WriteLine(string.Join("", line));
+            }
+        }
+
+        class Pixel
+        {
+            private char _color = ' ';
+
+            public void Apply(int layerColor)
+            {
+                switch (layerColor)
+                {
+                    case 0:
+                        _color = '.';
+                        break;
+
+                    case 1:
+                        _color = 'W';
+                        break;
+                }
+            }
+
+            public override string ToString()
+            {
+                return _color.ToString();
+            }
         }
 
         public static IEnumerable<IEnumerable<T>> Partition<T>(IEnumerable<T> items, int partitionSize)
