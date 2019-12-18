6b700b2531ac0da1c87204a680803dddeb3a4488
Kostas Stroggylos <modernist@users.noreply.github.com>
2019-12-19 01:42:17 +0200

    Day 12 Part 1

---
+++ 2019/aoc2019/Day12/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt")
+                .Select(line =>
+                    line.Split(new[] {' ', '<', '>', ',', '=', 'x', 'y', 'z'}, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(float.Parse).ToArray())
+                .Select(values => new Vector3(values[0], values[1], values[2]))
+                .Select(pos => new Moon()
+                {
+                    Position = pos,
+                    Velocity = new Vector3()
+                });
+
+            Console.WriteLine(Part1(input));
+
+            Console.Read();
+        }
+
+        static int Part1(IEnumerable<Moon> moons)
+        {
+            var state = Step(moons.ToArray()).Skip(999).First();
+            return state.Sum(moon => moon.Energy);
+        }
+
+        static IEnumerable<Moon[]> Step(Moon[] moons)
+        {
+            while (true)
+            {
+                foreach (var a in moons)
+                {
+                    foreach (var b in moons)
+                    {
+                        if (ReferenceEquals(a, b))
+                        {
+                            continue;
+                        }
+                        a.ApplyGravity(b);
+                    }
+                }
+
+                foreach (var moon in moons)
+                {
+                    moon.ApplyVelocity();
+                }
+
+                yield return moons;
+            }
+        }
+    }
+
+    public class Moon : IEquatable<Moon>
+    {
+        //Vector3 should be safe for x, y, z up to 2^24
+
+        public Vector3 Position;
+
+        public Vector3 Velocity;
+
+        public int PotentialEnergy
+        {
+            get
+            {
+                var abs = Vector3.Abs(Position);
+                return (int)(abs.X + abs.Y + abs.Z);
+            }
+        }
+
+        public int KineticEnergy
+        {
+            get
+            {
+                var abs = Vector3.Abs(Velocity);
+                return (int) (abs.X + abs.Y + abs.Z);
+            }
+        }
+
+        public int Energy => PotentialEnergy * KineticEnergy;
+
+        public void ApplyGravity(Moon other)
+        {
+            Velocity.X += Math.Sign(other.Position.X - Position.X);
+            Velocity.Y += Math.Sign(other.Position.Y - Position.Y);
+            Velocity.Z += Math.Sign(other.Position.Z - Position.Z);
+        }
+
+        public void ApplyVelocity()
+        {
+            Position += Velocity;
+        }
+
+        public bool Equals(Moon other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Position.Equals(other.Position) && Velocity.Equals(other.Velocity);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Moon) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Position.GetHashCode() * 397) ^ Velocity.GetHashCode();
+            }
+        }
+    }
+}
